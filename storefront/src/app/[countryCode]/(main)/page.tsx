86f75e844f55dbfdd401cb3b544b4389111c0e75
mykhailov0 <166093772+mykhailov0@users.noreply.github.com>
2025-04-24 15:08:33 +0300

    refactor: update page metadata title and description for ODESADISC

--- storefront/src/app/[countryCode]/(main)/page.tsx
+++ storefront/src/app/[countryCode]/(main)/page.tsx
@@ -6,9 +6,9 @@ import { getCollectionsWithProducts } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
 
 export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
+  title: "ODESADISC",
   description:
-    "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
+    "Інтернет-магазин вінілових платівок, CD-дисків та готових комплектів в Україні",
 }
 
 export default async function Home({
