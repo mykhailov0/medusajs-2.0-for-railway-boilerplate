782f6ffd4f9c627d5bb635a88c3d13821ad82774
mykhailov0 <166093772+mykhailov0@users.noreply.github.com>
2025-04-24 16:50:28 +0300

    refactor: clean up layout.tsx by standardizing import statements and formatting

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -1,24 +1,23 @@
 // src/app/layout.tsx
-import { getBaseURL } from "@lib/util/env"
-import { Metadata } from "next"
-import "styles/globals.css"
+import { getBaseURL } from "@lib/util/env";
+import { Metadata } from "next";
+import "styles/globals.css";
 
-import { MedusaAppProvider } from "src/providers/MedusaAppProvider"
+// Використовуємо клієнтський провайдер з хуками
+import { MedusaAppProvider } from "../providers/MedusaAppProvider";
 
 export const metadata: Metadata = {
   metadataBase: new URL(getBaseURL()),
-}
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
       <body>
         <MedusaAppProvider>
-          <main className="relative">
-            {children}
-          </main>
+          <main className="relative">{children}</main>
         </MedusaAppProvider>
       </body>
     </html>
-  )
+  );
 }
